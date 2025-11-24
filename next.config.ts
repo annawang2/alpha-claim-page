f40fd97125d7b1ea91d1c594931becdb5ec47caa
Anna Wang <annahuilin.wang@gmail.com>
2025-11-24 15:04:56 -0600

    Downgrade to Next.js 15 for build stability

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        // Exclude problematic test dependencies from thread-stream
-        'tap': false,
-        'tape': false,
-        'desm': false,
-        'fastbench': false,
-        'pino-elasticsearch': false,
-        'why-is-node-running': false,
-      },
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
