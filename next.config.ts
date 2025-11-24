869032e096c6495c0ac1ae264587f32c8c265291
Anna Wang <annahuilin.wang@gmail.com>
2025-11-24 14:44:47 -0600

    Configure webpack to ignore test files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Exclude test files from being bundled
+    config.module.rules.push({
+      test: /\.test\.(js|mjs|ts|tsx)$/,
+      loader: 'ignore-loader',
+    });
+    
+    return config;
+  },
+  // Tell Next.js to ignore certain patterns during build
+  transpilePackages: [],
 };
 
 export default nextConfig;
