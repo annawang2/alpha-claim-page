fa65277639eae008921ea6854b4ffe37b85cd836
Anna Wang <annahuilin.wang@gmail.com>
2025-11-24 14:48:52 -0600

    Add turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // Exclude test files from being bundled
-    config.module.rules.push({
-      test: /\.test\.(js|mjs|ts|tsx)$/,
-      loader: 'ignore-loader',
-    });
-    
-    return config;
-  },
-  // Tell Next.js to ignore certain patterns during build
-  transpilePackages: [],
+  turbopack: {}, // This tells Next.js we're okay using Turbopack
 };
 
 export default nextConfig;
